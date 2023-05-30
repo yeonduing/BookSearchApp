2cafb221831081364d894c9256e4674986364f1e
yeonduing <yeonduing@gmail.com>
2023-05-31 01:33:54 +0900

    add UIFont extension

---
+++ BookSearchApp/BookSearchApp/Extensions/UIFont+.swift
@@ -0,0 +1,32 @@
+//
+//  UIFont+.swift
+//  BookSearchApp
+//
+//  Created by itzel.du on 2023/05/31.
+//
+
+import UIKit
+
+extension UIFont {
+
+  static let thumbnailTitle = UIFont.systemFont(ofSize: 16, weight: .bold)
+  static let thumbnailSubtitle = UIFont.systemFont(ofSize: 12, weight: .medium)
+  static let thumbnailIsbn13 = UIFont.systemFont(ofSize: 9, weight: .medium)
+  static let thumbnailPrice = UIFont.systemFont(ofSize: 24, weight: .bold)
+  static let thumbnailURL = UIFont.systemFont(ofSize: 9, weight: .medium)
+
+  static let queryText = UIFont.systemFont(ofSize: 21, weight: .bold)
+
+  static let detailTitle = UIFont.systemFont(ofSize: 26, weight: .bold)
+  static let detailSubtitle = UIFont.systemFont(ofSize: 17, weight: .medium)
+  static let detailAuthor = UIFont.systemFont(ofSize: 15, weight: .medium)
+  static let detailPrice = UIFont.systemFont(ofSize: 24, weight: .bold)
+  static let detailPublisher = UIFont.systemFont(ofSize: 17, weight: .medium)
+  static let detailDescription = UIFont.systemFont(ofSize: 16, weight: .medium)
+  static let detailIsbn = UIFont.systemFont(ofSize: 14, weight: .medium)
+  static let detailETCLarge = UIFont.systemFont(ofSize: 16, weight: .bold)
+  static let detailETC = UIFont.systemFont(ofSize: 14, weight: .medium)
+
+  static let emptyResultTitle = UIFont.systemFont(ofSize: 16, weight: .medium)
+  static let emptyResultDescription = UIFont.systemFont(ofSize: 14, weight: .medium)
+}
