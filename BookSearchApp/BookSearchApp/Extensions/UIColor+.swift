d2b67335b36f0cae40199c6d6f780e5ce97575ac
yeonduing <yeonduing@gmail.com>
2023-05-31 01:31:45 +0900

    add color assets

---
+++ BookSearchApp/BookSearchApp/Extensions/UIColor+.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+.swift
+//  BookSearchApp
+//
+//  Created by itzel.du on 2023/05/31.
+//
+
+import UIKit
+
+extension UIColor {
+
+  static let background = UIColor(named: "search-background")
+  static let detailBackground = UIColor(named: "detail-background")
+  static let cellBackground = UIColor(named: "cell-background")
+  static let main = UIColor(named: "main")
+  static let primary = UIColor(named: "primary")
+  static let secondary = UIColor(named: "secondary")
+  static let tertiary = UIColor(named: "tertiary")
+  static let shadow = UIColor(named: "shadow")
+  static let leftGradient = UIColor(named: "left-gradient")
+  static let rightGradient = UIColor(named: "right-gradient")
+  static let optionButton = UIColor(named: "option-button")
+  static let optionButtonTint = UIColor(named: "option-button-tint")
+  static let optionButtonDisable = UIColor(named: "option-button-disable")
+}
