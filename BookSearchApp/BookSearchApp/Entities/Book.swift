f5b3e7847fbcfafc5917dcc52aa741b2f392c1ab
yeonduing <yeonduing@gmail.com>
2023-05-31 13:12:12 +0900

    add Book entity

---
+++ BookSearchApp/BookSearchApp/Entities/Book.swift
@@ -0,0 +1,18 @@
+//
+//  Book.swift
+//  BookSearchApp
+//
+//  Created by itzel.du on 2023/05/31.
+//
+
+import Foundation
+
+struct Book {
+
+  let title: String
+  let subtitle: String
+  let isbn13: String
+  let price: String
+  let imageURLString: String
+  let urlString: String
+}
