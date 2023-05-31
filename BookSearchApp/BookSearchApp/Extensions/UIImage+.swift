f68dc337c9be0bd5ad5a5e6b40c124c9b95b1120
yeonduing <yeonduing@gmail.com>
2023-05-31 12:02:05 +0900

    add UIImage extension

---
+++ BookSearchApp/BookSearchApp/Extensions/UIImage+.swift
@@ -0,0 +1,26 @@
+//
+//  UIImage+.swift
+//  BookSearchApp
+//
+//  Created by itzel.du on 2023/05/31.
+//
+
+import UIKit
+
+extension UIImage {
+
+  static let magnifyingglassCircle = UIImage(systemName: "magnifyingglass.circle")
+  static let magnifyingglassCircleFill = UIImage(systemName: "magnifyingglass.circle.fill")
+  static let bookmark = UIImage(systemName: "bookmark")
+  static let bookmarkFill = UIImage(systemName: "bookmark.fill")
+  static let chevronLeft = UIImage(systemName: "chevron.left")
+  static let normalMode = UIImage(systemName: "rectangle.grid.1x2.fill")
+  static let minimalMode = UIImage(systemName: "rectangle.grid.3x2.fill")
+  static let url = UIImage(systemName: "arrowshape.turn.up.forward")
+  static let urlFill = UIImage(systemName: "arrowshape.turn.up.forward.fill")
+
+  static let star = UIImage(named: "star")
+  static let starFill = UIImage(named: "star.fill")
+  static let pdf = UIImage(named: "pdf")
+  static let pdfDisable = UIImage(named: "pdf-disable")
+}
