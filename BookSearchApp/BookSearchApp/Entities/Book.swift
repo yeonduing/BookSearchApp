3c27bc3ce74ea225ce6a71d7d5ac0535508bc62e
yeonduing <yeonduing@gmail.com>
2023-06-03 14:28:38 +0900

    add DetailBook structures

--- BookSearchApp/BookSearchApp/Entities/Book.swift
+++ BookSearchApp/BookSearchApp/Entities/Book.swift
@@ -28,3 +28,48 @@ extension Book {
     urlString = searchBookResponse.url
   }
 }
+
+struct BookDetail {
+
+  let title: String
+  let subtitle: String
+  let authors: String
+  let publisher: String
+  let isbn10: String
+  let isbn13: String
+  let pages: String
+  let year: String
+  let rating: Int
+  let descriptionString: String
+  let price: String
+  let imageURLString: String
+  let urlString: String
+  let pdf: [String: String]?
+}
+
+extension BookDetail {
+
+  init(bookResponse: BookResponse) {
+    title = bookResponse.title
+    subtitle = bookResponse.subtitle
+    authors = bookResponse.authors
+    publisher = bookResponse.publisher
+    isbn10 = bookResponse.isbn10
+    isbn13 = bookResponse.isbn13
+    pages = bookResponse.pages
+    year = bookResponse.year
+    rating = Int(bookResponse.rating) ?? 0
+    descriptionString = bookResponse.desc
+    price = bookResponse.price
+    imageURLString = bookResponse.image
+    urlString = bookResponse.url
+    pdf = bookResponse.pdf
+  }
+}
+
+struct BookSummary {
+
+  let rating: String
+  let pages: String
+  let year: String
+}
